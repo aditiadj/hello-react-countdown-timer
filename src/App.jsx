60e11fc51cd1fdf01eebc82c11240806b878db26
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-02-05 14:55:57 +0700

    Set countdown date January 1, 2019

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
+import Clock from './Clock'
 import './App.css'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      deadline: 'January 1, 2019'
+      deadline: 'January 1, 2019',
+      newDeadline: ''
     }
   }
 
   changeDeadline() {
-    this.setState({deadline: 'December 1, 2018'})
+    this.setState({deadline: this.state.newDeadline});
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-title">Countdown {this.state.deadline}</div>
+        <Clock
+          deadline={this.state.deadline}
+          />
         <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">20 hours</div>
-          <div className="Clock-minutes">10 minutes</div>
-          <div className="Clock-seconds">5 seconds</div>
-        </div>
-        <div>
-          <input placeholder='new date'/>
+          <input
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
           <button onClick={() => this.changeDeadline()}>
             Submit
           </button>
